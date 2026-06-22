53398cdb0fadd92720df05c8af6fb93f3d59aee7
zexk <bouraouiochi@gmail.com>
2026-06-23 00:17:00 +0200

    flake: flat install layout, add win32 package; ci: add win32 job

--- flake.nix
+++ flake.nix
@@ -11,19 +11,52 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        mingw = pkgs.pkgsCross.mingwW64;
       in
       {
-        packages.default = kiln.lib.mkKilnGame {
-          inherit pkgs;
-          pname = "kyub";
-          version = "0.1.0";
-          src = ./.;
-          meta = {
-            description = "Minimalist voxel game with Vulkan renderer";
-            homepage = "https://github.com/zexk/kyub";
-            license = pkgs.lib.licenses.mit;
-            platforms = pkgs.lib.platforms.linux;
-            mainProgram = "kyub";
+        packages = {
+          default = kiln.lib.mkKilnGame {
+            inherit pkgs;
+            pname = "kyub";
+            version = "0.1.0";
+            src = ./.;
+            meta = {
+              description = "Minimalist voxel game with Vulkan renderer";
+              homepage = "https://github.com/zexk/kyub";
+              license = pkgs.lib.licenses.mit;
+              platforms = pkgs.lib.platforms.linux;
+              mainProgram = "kyub";
+            };
+            installPhase = ''
+              mkdir -p $out/bin
+              cp kyub $out/bin/
+              cp -r shaders $out/bin/
+              cp -r assets $out/bin/
+            '';
+          };
+
+          win32 = mingw.stdenv.mkDerivation {
+            pname = "kyub-win32";
+            version = "0.1.0";
+            src = ./.;
+            nativeBuildInputs = with pkgs; [ cmake ninja pkg-config shaderc gcc ];
+            buildInputs = with mingw; [
+              vulkan-headers
+              vulkan-loader
+              windows.pthreads
+              windows.mcfgthreads
+              stb
+            ];
+            cmakeFlags = [ "-DKILN_DIR=${kiln.outPath}" "-DBUILD_TESTING=OFF" ];
+            buildPhase = "cmake --build . --target kyub";
+            installPhase = ''
+              mkdir -p $out/bin
+              cp kyub.exe $out/bin/
+              cp -r shaders $out/bin/
+              cp -r assets $out/bin/
+              cp ${mingw.windows.mcfgthreads}/bin/libmcfgthread-2.dll $out/bin/
+            '';
+            doCheck = false;
           };
         };
 
@@ -36,7 +69,7 @@
           ];
           shellHook = ''
             export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
-            echo "Kyub dev shell: cmake -B build -G Ninja -DKILN_DIR=${kiln} && cmake --build build"
+            echo "Kyub dev shell: cmake -B build -G Ninja -DKILN_DIR=${kiln.outPath} && cmake --build build"
           '';
         };
       });
